14f6819c210e8b946e3b57d04efc2209bed80957
Zougui <zougui.dev@protonmail.com>
2024-08-04 16:27:33 +0200

    updated navigation menu item/link styles

--- src/components/molecules/NavigationMenu/styles.ts
+++ src/components/molecules/NavigationMenu/styles.ts
@@ -2,6 +2,7 @@ import { tv } from 'tailwind-variants';
 
 export const triggerStyle = tv({
   base: [
+    'text-md sm:text-lg',
     'group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors',
     'hover:bg-accent hover:text-accent-foreground',
     'focus:bg-accent focus:text-accent-foreground focus:outline-none',
