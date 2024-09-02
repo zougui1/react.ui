21c5be20390a587b57a0ba60f4e2d7ed0121e5db
Zougui <zougui.dev@protonmail.com>
2024-09-02 22:49:44 +0200

    MainLayout: removed DataTable compound component

--- src/components/templates/MainLayout/index.ts
+++ src/components/templates/MainLayout/index.ts
@@ -1,13 +1,11 @@
 import { MainLayoutBody, type MainLayoutBodyProps } from './MainLayoutBody';
 import { MainLayoutContainer, type MainLayoutContainerProps } from './MainLayoutContainer';
-import { MainLayoutDataTable, type MainLayoutDataTableProps } from './MainLayoutDataTable';
 import { MainLayoutScreen, type MainLayoutScreenProps } from './MainLayoutScreen';
 import { MainLayoutSidePanel, type MainLayoutSidePanelProps } from './MainLayoutSidePanel';
 
 export const MainLayout = {
   Body: MainLayoutBody,
   Container: MainLayoutContainer,
-  DataTable: MainLayoutDataTable,
   Screen: MainLayoutScreen,
   SidePanel: MainLayoutSidePanel,
 };
@@ -15,7 +13,6 @@ export const MainLayout = {
 export type {
   MainLayoutBodyProps,
   MainLayoutContainerProps,
-  MainLayoutDataTableProps,
   MainLayoutScreenProps,
   MainLayoutSidePanelProps,
 };
