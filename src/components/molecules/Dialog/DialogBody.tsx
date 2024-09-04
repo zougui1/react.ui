3725890c09b9d2729beea34a9dfcebcd39059236
Zougui <zougui.dev@protonmail.com>
2024-09-04 17:19:01 +0200

    Dialog.Body: decreased max-height by 40px to add space on top and bottom of dialog for better UI, switched from inline stlye to className for easier max-height override via className

--- src/components/molecules/Dialog/DialogBody.tsx
+++ src/components/molecules/Dialog/DialogBody.tsx
@@ -3,15 +3,14 @@ import React from 'react';
 import { cn } from '../../../utils';
 
 export const DialogBody = React.forwardRef<HTMLDivElement, DialogBodyProps>(
-  ({ className, style, ...rest }, ref) => (
+  ({ className, ...rest }, ref) => (
     <div
       {...rest}
       ref={ref}
-      className={cn('overflow-y-auto py-6 pl-1 pr-4', className)}
-      style={{
-        maxHeight: 'min(calc(100vh - 150px), 56rem)',
-        ...style,
-      }}
+      className={cn(
+        'overflow-y-auto py-6 pl-1 pr-4 max-h-[min(calc(100vh-190px),_56rem)]',
+        className,
+      )}
     />
   ),
 );
