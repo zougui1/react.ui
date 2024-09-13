97e7207555ead74292e0fb84062ccbd851ec7558
Zougui <zougui.dev@protonmail.com>
2024-09-13 23:07:15 +0200

    Button: removed useless function

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { tv, type VariantProps } from 'tailwind-variants';
 
-import { cn } from '../../utils';
-
 export const buttonVariants = tv({
   base: [
     'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors',
@@ -38,7 +36,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={buttonVariants({ variant, size, className })}
         ref={ref}
         {...props}
       />
